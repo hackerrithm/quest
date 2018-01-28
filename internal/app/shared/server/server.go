a4d31e90f61b38083fa09d2587852835ab0e29c2
hackerrithm <kemargalloway@gmail.com>
2018-01-28 02:08:38 -0500

    Add simple server

---
+++ internal/app/shared/server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func greetTheWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello New App!")
+}
+
+// Server starts the app on a port
+func Server() {
+	http.HandleFunc("/", greetTheWorld)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
